730a7e051efd2217d38d0632a1487082e220cc87
Zeno <zeno.casellato@tuwien.ac.at>
2021-06-24 18:13:35 +0200

    fits changes proposal, refs #64937

--- src/main/java/at/ac/tuwien/rest/fits/dto/generated/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://hul.harvard.edu/ois/xml/ns/fits/fits_output", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package at.ac.tuwien.rest.fits.dto.generated;
