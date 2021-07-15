a12f094525f4fcc410c409f804ed905a6822652b
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-07-15 14:40:42 +0200

    [Repo] Add search API, refs #64938

--- src/main/java/at/ac/tuwien/r3data/RepositoriesResource.java
+++ src/main/java/at/ac/tuwien/r3data/RepositoriesResource.java
@@ -3,13 +3,18 @@ package at.ac.tuwien.r3data;
 import generated.Repository;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import org.re3data.schema._2_2.Re3Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 import java.util.List;
 
 @Path("/repositories")
@@ -17,15 +22,30 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class RepositoriesResource {
 
-    @Inject RepositoriesService repositoriesService;
+    private static final Logger log = LoggerFactory.getLogger(RepositoriesResource.class);
+
+    @Inject
+    RepositoriesService repositoriesService;
 
     @GET
     public List<Repository> getAll() {
+        log.info("Get all repositories");
         return repositoriesService.getAll();
     }
 
     @GET
     @Path("/{id}")
-    public Re3Data getById(@PathParam String id) { return repositoriesService.getById(id); }
+    public Re3Data getById(@PathParam String id) {
+        log.info("Get repository with id: {}", id);
+        return repositoriesService.getById(id);
+    }
+
+    @GET
+    @Path("/search")
+    public List<Repository> search(@Context UriInfo uriInfo) {
+        log.info("Search repositories: {}", uriInfo.getQueryParameters());
+        MultivaluedMap<String, String> params = uriInfo.getQueryParameters();
+        return repositoriesService.search(params);
+    }
 
 }
--- src/main/java/at/ac/tuwien/r3data/RepositoriesService.java
+++ src/main/java/at/ac/tuwien/r3data/RepositoriesService.java
@@ -6,6 +6,7 @@ import org.re3data.schema._2_2.Re3Data;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.core.MultivaluedMap;
 import java.util.List;
 
 @ApplicationScoped
@@ -23,6 +24,27 @@ public class RepositoriesService {
 
     public Re3Data getById(String id) {
         return repositoriesRemoteResource.getById(id);
+    }
+
+    public List<Repository> search(MultivaluedMap<String, String> params) {
+        List<String> subjects = params.get("subjects");
+        List<String> contentTypes = params.get("contentTypes");
+        List<String> certificates = params.get("certificates");
+        List<String> countries = params.get("countries");
+        List<String> pidSystems = params.get("pidSystems");
+        List<String> aidSystems = params.get("aidSystems");
+        List<String> repositoryAccess = params.get("repositoryAccess");
+        List<String> dataAccess = params.get("dataAccess");
+        List<String> dataUpload = params.get("dataUpload");
+        List<String> dataLicenses = params.get("dataLicenses");
+        List<String> repositoryTypes = params.get("repositoryTypes");
+        List<String> institutionTypes = params.get("institutionTypes");
+        List<String> versioning = params.get("versioning");
+        List<String> metadataStandards = params.get("metadataStandards");
+        return repositoriesRemoteResource.search(
+                subjects, contentTypes, countries, certificates, pidSystems,
+                aidSystems, repositoryAccess, dataAccess, dataUpload, dataLicenses,
+                repositoryTypes, institutionTypes, versioning, metadataStandards);
     }
 
 }
