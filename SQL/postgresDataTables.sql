b633092f03c93aab9f5a998a79c47db6e9a7fa25
Zeno <zeno.casellato@tuwien.ac.at>
2021-07-27 18:10:30 +0200

    added dataset-host distributions, fixes #68573 #70185

--- SQL/postgresDataTables.sql
+++ SQL/postgresDataTables.sql
@@ -437,7 +437,6 @@ CREATE TABLE damap.dataset
     id bigint NOT NULL,
 	version integer NOT NULL,
 	dmp_id bigint,
-	host_id bigint,
     title text,
     type text,
     data_size text,
@@ -452,11 +451,6 @@ CREATE TABLE damap.dataset
     PRIMARY KEY (id),
     FOREIGN KEY (dmp_id)
         REFERENCES damap.dmp (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-	FOREIGN KEY (host_id)
-        REFERENCES damap.host (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
@@ -578,6 +572,31 @@ CREATE TABLE damap.access_management
 );
 
 ALTER TABLE damap.access_management
+    OWNER to damap;
+
+
+--------------------------------------------------------------
+
+CREATE TABLE damap.distribution
+(
+    id bigint NOT NULL,
+	version integer NOT NULL,
+	dataset_id bigint,
+	host_id bigint,
+    PRIMARY KEY (id),
+    FOREIGN KEY (dataset_id)
+        REFERENCES damap.dataset (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+	FOREIGN KEY (host_id)
+        REFERENCES damap.host (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+ALTER TABLE damap.distribution
     OWNER to damap;
 
 
