d42e3f4af2a87e57cd97a965f4c9beecf69de7af
Zeno <zeno.casellato@tuwien.ac.at>
2021-07-28 14:09:11 +0200

    annotation @Data for DOs, fixes #70756

--- src/main/java/at/ac/tuwien/damap/rest/domain/FundingDO.java
+++ src/main/java/at/ac/tuwien/damap/rest/domain/FundingDO.java
@@ -2,11 +2,9 @@ package at.ac.tuwien.damap.rest.domain;
 
 import at.ac.tuwien.damap.enums.EFundingState;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FundingDO {
 
