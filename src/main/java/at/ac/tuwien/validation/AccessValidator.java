16bdbbefe3d9bca02233d5dab07a274ed1104825
Clara Schuster <clara.schuster@tuwien.ac.at>
2021-08-12 10:53:43 +0200

    [Dmp API] Validate access for getDmpById & updateDmp, refs #69761

--- src/main/java/at/ac/tuwien/validation/AccessValidator.java
+++ src/main/java/at/ac/tuwien/validation/AccessValidator.java
@@ -4,15 +4,17 @@ import at.ac.tuwien.damap.domain.Access;
 import at.ac.tuwien.damap.enums.EFunctionRole;
 import at.ac.tuwien.damap.repo.AccessRepo;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.List;
 
+@ApplicationScoped
 public class AccessValidator {
 
     @Inject
     AccessRepo accessRepo;
 
-    public boolean canViewDmp(Long dmpId, String personId) {
+    public boolean canViewDmp(long dmpId, String personId) {
         List<Access> accessList = accessRepo.getAllDmpByUniversityId(personId);
 
         for (Access access : accessList) {
@@ -24,7 +26,7 @@ public class AccessValidator {
         return false;
     }
 
-    public boolean canEditDmp(Long dmpId, String personId) {
+    public boolean canEditDmp(long dmpId, String personId) {
         List<Access> accessList = accessRepo.getAllDmpByUniversityId(personId);
 
         for (Access access : accessList) {
@@ -40,7 +42,7 @@ public class AccessValidator {
         return false;
     }
 
-    public boolean canExportDmp(Long dmpId, String personId) {
+    public boolean canExportDmp(long dmpId, String personId) {
         return this.canViewDmp(dmpId, personId);
     }
 }
