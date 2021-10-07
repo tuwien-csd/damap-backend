c140b7cc4ae8102b1c8c7127a7a2ec38468d5f8b
Zeno <zeno.casellato@tuwien.ac.at>
2021-10-07 16:58:09 +0200

    track person who edited dmp, fixes #72256

--- src/main/java/at/ac/tuwien/damap/domain/listener/DamapRevisionListener.java
+++ src/main/java/at/ac/tuwien/damap/domain/listener/DamapRevisionListener.java
@@ -1,17 +1,23 @@
 package at.ac.tuwien.damap.domain.listener;
 
 import at.ac.tuwien.damap.domain.DamapRevisionEntity;
+import at.ac.tuwien.damap.security.SecurityService;
+import lombok.extern.jbosslog.JBossLog;
 import org.hibernate.envers.RevisionListener;
 
+import javax.enterprise.inject.spi.CDI;
+
+@JBossLog
 public class DamapRevisionListener implements RevisionListener {
 
     @Override
     public void newRevision(Object revisionEntity) {
+        if (revisionEntity instanceof DamapRevisionEntity) {
 
-        DamapRevisionEntity damapRevisionEntity = (DamapRevisionEntity) revisionEntity;
-
-        //TODO determine what/who performed the change and insert into revision table
-
-//        damapRevisionEntity.setChanged_by( change_by );
+            final String userId = CDI.current().select(SecurityService.class).get().getUserId();
+            final String username = CDI.current().select(SecurityService.class).get().getUserName();
+            ((DamapRevisionEntity) revisionEntity).setChangedById(userId);
+            ((DamapRevisionEntity) revisionEntity).setChangedBy(username);
+        }
     }
 }
