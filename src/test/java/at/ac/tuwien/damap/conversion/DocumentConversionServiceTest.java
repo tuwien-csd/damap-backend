b1e6f6f0462a8eefe945668312520ecea2598815
Hardani Maulana <hardani.maulana@tugraz.at>
2021-12-16 15:48:58 +0100

    Update test classes by adding assertion to define what should happen

--- src/test/java/at/ac/tuwien/damap/conversion/DocumentConversionServiceTest.java
+++ src/test/java/at/ac/tuwien/damap/conversion/DocumentConversionServiceTest.java
@@ -3,9 +3,11 @@ package at.ac.tuwien.damap.conversion;
 import io.quarkus.test.junit.QuarkusTest;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 import javax.enterprise.inject.Instance;
+import java.util.*;
 
 @QuarkusTest
 
@@ -15,11 +17,36 @@ public class DocumentConversionServiceTest {
     Instance<DocumentConversionService> documentConversionServices;
 
     @Test
-    public void testEmptyDmp() throws Exception {
+    public void testLoadTemplate() throws Exception {
         for (DocumentConversionService documentConversionService : documentConversionServices) {
-
-            System.out.println("Test passed");
+            //testing load template
+            //TODO: generate blank document as a test
             XWPFDocument document = documentConversionService.loadTemplate("template/template.docx");
+            Assertions.assertNotNull(document);
+
+            //testing multiple variable handling
+            String result1 = documentConversionService.multipleVariable(testTwoVariable());
+            String result2 = documentConversionService.multipleVariable(testThreeVariable());
+            Assertions.assertEquals(twoVariables(), result1);
+            Assertions.assertEquals(threeVariables(), result2);
         }
+    }
+
+    private List<String> testTwoVariable() {
+        List<String> variables = Arrays.asList("var1", "var2");
+        return variables;
+    }
+
+    private List<String> testThreeVariable() {
+        List<String> variables = Arrays.asList("var1", "var2", "var3");
+        return variables;
+    }
+
+    private String twoVariables() {
+        return "var1 and var2";
+    }
+
+    private String threeVariables() {
+        return "var1, var2, var3";
     }
 }
