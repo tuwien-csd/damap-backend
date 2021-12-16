451edfceea6a4e8a838661d8bc2accc75df0da1a
Hardani Maulana <hardani.maulana@tugraz.at>
2021-12-16 15:48:58 +0100

    Update test classes by adding assertion to define what should happen

--- src/test/java/at/ac/tuwien/damap/conversion/ExportFWFTemplateTest.java
+++ src/test/java/at/ac/tuwien/damap/conversion/ExportFWFTemplateTest.java
@@ -5,6 +5,7 @@ import at.ac.tuwien.damap.repo.DmpRepo;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -32,7 +33,9 @@ public class ExportFWFTemplateTest {
     public void testEmptyDmp() throws Exception {
         Long id = 123L;
 
+        //testing the export document return not a null document
         XWPFDocument document = exportFWFTemplate.exportTemplate(id);
+        Assertions.assertNotNull(document);
     }
 
     private Dmp createDmp() {
