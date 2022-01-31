1cc447e76b904cb471a3542b4f2d83e4b248ba72
Hardani Maulana <hardani.maulana@tugraz.at>
2022-01-31 11:03:10 +0100

    [DMP] add backend service and database for consent form refs #75211

---
+++ src/main/java/at/ac/tuwien/damap/rest/ConsentResource.java
@@ -0,0 +1,43 @@
+package at.ac.tuwien.damap.rest;
+
+import at.ac.tuwien.damap.rest.administration.domain.ConsentDO;
+import at.ac.tuwien.damap.rest.administration.service.ConsentService;
+import at.ac.tuwien.damap.security.SecurityService;
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.AuthenticationFailedException;
+import lombok.extern.jbosslog.JBossLog;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/api/consent")
+@Authenticated
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@JBossLog
+public class ConsentResource {
+
+    @Inject
+    ConsentService consentService;
+
+    @Inject
+    SecurityService securityService;
+
+    @GET
+    @RolesAllowed("user")
+    public Boolean consent() {
+        log.info("User consent");
+        return consentService.getConsentByUser(this.getPersonId()) ;
+    }
+
+    private String getPersonId() {
+        if (securityService == null) {
+            throw new AuthenticationFailedException("User ID is missing.");
+        }
+        return securityService.getUserId();
+    }
+
+}
---
+++ src/main/java/at/ac/tuwien/damap/rest/administration/service/ConsentService.java
@@ -0,0 +1,17 @@
+package at.ac.tuwien.damap.rest.administration.service;
+
+import at.ac.tuwien.damap.domain.Consent;
+import at.ac.tuwien.damap.rest.administration.domain.ConsentDO;
+import at.ac.tuwien.damap.rest.administration.mapper.ConsentDOMapper;
+
+public class ConsentService {
+
+//    public ConsentDO getConsentById(long consentId) {
+//        return ConsentDOMapper.mapEntityToDO(, new ConsentDO())
+//                mapEntityToDO(dmpRepo.findById(dmpId), new DmpDO());
+//    }
+
+    public Boolean getConsentByUser(String universityId) {
+        return false;
+    }
+}
