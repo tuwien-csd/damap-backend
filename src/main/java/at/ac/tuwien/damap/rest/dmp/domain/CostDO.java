585cfe91e55367a435e95204ff7ca09128c9da4e
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-01-31 16:58:39 +0100

    Use enum variable instead of value for be-fe communication
    refs #74831

--- src/main/java/at/ac/tuwien/damap/rest/dmp/domain/CostDO.java
+++ src/main/java/at/ac/tuwien/damap/rest/dmp/domain/CostDO.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.damap.rest.dmp.domain;
 
+import at.ac.tuwien.damap.enums.ECostType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
@@ -12,6 +13,6 @@ public class CostDO {
     private Float value;
     private String currencyCode; // controlled vocabulary: ISO 4217
     private String description;
-    private String type;
+    private ECostType type;
     private String customType;
 }
