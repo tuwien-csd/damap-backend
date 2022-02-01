8ca44ca883e6a8fc8e7ab8ebe7e478eeb0ca5ee3
Hardani Maulana <hardani.maulana@tugraz.at>
2022-02-01 11:47:06 +0100

    [bug fixes] update backend service for consent by adding repo and post method refs #75211

--- src/main/java/at/ac/tuwien/damap/domain/Consent.java
+++ src/main/java/at/ac/tuwien/damap/domain/Consent.java
@@ -1,6 +1,8 @@
 package at.ac.tuwien.damap.domain;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.envers.Audited;
 
 import javax.persistence.Column;
@@ -8,10 +10,11 @@ import javax.persistence.Entity;
 import java.util.Date;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Audited
 
-public class Consent {
+public class Consent extends PanacheEntity {
 
     @Column(name = "university_id")
     private String universityId;
