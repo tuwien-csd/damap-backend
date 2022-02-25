4589cbc62bdce462c1d6f7c41c0ade787b717178
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-02-25 14:14:45 +0100

    [bugfix] madmp export now correlates datasets and hosts correctly,
    fixes #76181

--- src/main/java/at/ac/tuwien/damap/rest/madmp/mapper/MaDmpMapper.java
+++ src/main/java/at/ac/tuwien/damap/rest/madmp/mapper/MaDmpMapper.java
@@ -127,15 +127,21 @@ public class MaDmpMapper {
         dataset.setDescription(datasetDO.getComment());
 
         List<Distribution> distributionList = new ArrayList<>();
-        dmpDO.getHosts().forEach(hostDO -> {
+        dmpDO.getHosts().stream().filter(hostDO ->
+            hostDO.getDatasets().contains(datasetDO.getReferenceHash())
+        ).forEach(hostDO -> {
             Distribution distribution = mapToMaDmp(datasetDO, new Distribution());
             distributionList.add(mapToMaDmpFromHost(hostDO, distribution, repositoriesService));
         });
-        dmpDO.getStorage().forEach(storageDO -> {
+        dmpDO.getStorage().stream().filter(hostDO ->
+                hostDO.getDatasets().contains(datasetDO.getReferenceHash())
+        ).forEach(storageDO -> {
             Distribution distribution = mapToMaDmp(datasetDO, new Distribution());
             distributionList.add(mapToMaDmpFromStorage(storageDO, distribution));
         });
-        dmpDO.getExternalStorage().forEach(storageDO -> {
+        dmpDO.getExternalStorage().stream().filter(hostDO ->
+                hostDO.getDatasets().contains(datasetDO.getReferenceHash())
+        ).forEach(storageDO -> {
             Distribution distribution = mapToMaDmp(datasetDO, new Distribution());
             distributionList.add(mapToMaDmpFromStorage(storageDO, distribution));
         });
