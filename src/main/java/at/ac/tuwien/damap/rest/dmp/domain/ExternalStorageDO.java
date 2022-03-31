ecf3bd9e00a6b6709e393d7fdbf6518f025f2382
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-03-31 16:02:58 +0200

    [task] implement simple DMP validation,
    refs #70296

--- src/main/java/at/ac/tuwien/damap/rest/dmp/domain/ExternalStorageDO.java
+++ src/main/java/at/ac/tuwien/damap/rest/dmp/domain/ExternalStorageDO.java
@@ -3,12 +3,18 @@ package at.ac.tuwien.damap.rest.dmp.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import javax.validation.constraints.Size;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ExternalStorageDO extends HostDO {
 
+    @Size(max = 255)
     private String url;
+    @Size(max = 255)
     private String backupFrequency;
+    @Size(max = 255)
     private String storageLocation;
+    @Size(max = 255)
     private String backupLocation;
 }
