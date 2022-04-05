27b4da12163b1ea03904387a830cccacd62187a8
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-04-05 10:44:28 +0200

    [task] generalization of public project,
    fixes #77172

---
+++ src/main/java/at/ac/tuwien/damap/conversion/TemplateFileBrokerService.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.damap.conversion;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public interface TemplateFileBrokerService {
+
+    Properties getScienceEuropeTemplateResource();
+
+    InputStream loadScienceEuropeTemplate();
+}
