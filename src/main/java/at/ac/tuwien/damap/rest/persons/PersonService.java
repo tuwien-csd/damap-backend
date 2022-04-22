b674c0def80a866636e91d20d71d0d78e34a8610
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-04-22 15:45:04 +0200

    add search API for institutional person search,
    refs #65895

--- src/main/java/at/ac/tuwien/damap/rest/persons/PersonService.java
+++ src/main/java/at/ac/tuwien/damap/rest/persons/PersonService.java
@@ -2,6 +2,10 @@ package at.ac.tuwien.damap.rest.persons;
 
 import at.ac.tuwien.damap.rest.dmp.domain.ContributorDO;
 
+import java.util.List;
+
 public interface PersonService {
     ContributorDO getPersonById(String id);
+
+    List<ContributorDO> getPersonSearchResult(String searchTerm);
 }
