67433944c8138320d3b5208993375257eae5d978
Clara Schuster <98585998+clarascu@users.noreply.github.com>
2022-08-16 14:07:34 +0200

    Set DataGeneration for new datasets only
    refs #79677

--- src/main/java/at/ac/tuwien/damap/rest/dmp/service/DmpConsistencyUtility.java
+++ src/main/java/at/ac/tuwien/damap/rest/dmp/service/DmpConsistencyUtility.java
@@ -24,6 +24,7 @@ public class DmpConsistencyUtility {
         // Remove datasets by source if not specified
         if (dmpDO.getDataKind() != EDataKind.SPECIFY) {
             removeDatasetsBySource(dmpDO, EDataSource.NEW);
+            dmpDO.setDataGeneration(null);
         }
         if (dmpDO.getReusedDataKind() != EDataKind.SPECIFY) {
             removeDatasetsBySource(dmpDO, EDataSource.REUSED);
@@ -71,7 +72,6 @@ public class DmpConsistencyUtility {
      */
     private void removeDatasetRelatedInfo(DmpDO dmpDO) {
         dmpDO.setDatasets(new ArrayList<>());
-        dmpDO.setDataGeneration(null);
         dmpDO.setDataRightsAndAccessControl(null);
 
         resetDocumentation(dmpDO);
