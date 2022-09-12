97c0ca5880889370a1bee3b7b0177b7201d8bef1
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-09-12 15:46:11 +0200

    add config test,
    refs #77459

---
+++ src/test/java/at/ac/tuwien/damap/rest/ConfigResourceTest.java
@@ -0,0 +1,26 @@
+package at.ac.tuwien.damap.rest;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+@TestHTTPEndpoint(ConfigResource.class)
+public class ConfigResourceTest {
+
+    @ConfigProperty(name = "damap.auth.frontend.url")
+    String authUrl;
+
+    @Test
+    public void testGetConfigEndpoint() {
+        given()
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("authUrl", is(authUrl));
+    }
+}
