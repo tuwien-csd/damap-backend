dd0c89762ecfb1f51e1a9e2152164f4779579e77
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-09-12 14:55:20 +0200

    add re3data tests,
    refs #77459

--- src/test/java/at/ac/tuwien/damap/r3data/RepositoriesResourceTest.java
+++ src/test/java/at/ac/tuwien/damap/r3data/RepositoriesResourceTest.java
@@ -1,15 +1,19 @@
 package at.ac.tuwien.damap.r3data;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import io.quarkus.test.security.jwt.Claim;
+import io.quarkus.test.security.jwt.JwtSecurity;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
 
 @QuarkusTest
 public class RepositoriesResourceTest {
 
     @Test
-    public void testRepositoriesEndpoint() {
+    public void testRepositoriesEndpoint_Invalid() {
         given()
                 .when().get("/api/repositories")
                 .then()
@@ -17,7 +21,15 @@ public class RepositoriesResourceTest {
     }
 
     @Test
-    public void testRepositoryByIdEndpoint() {
+    public void testRepositoriesRecommendedEndpoint_Invalid() {
+        given()
+                .when().get("/api/repositories/recommended")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    public void testRepositoryByIdEndpoint_Invalid() {
         given()
                 .when().get("/api/repositories/1")
                 .then()
@@ -25,11 +37,60 @@ public class RepositoriesResourceTest {
     }
 
     @Test
-    public void testRepositoriesSearchEndpoint() {
+    public void testRepositoriesSearchEndpoint_Invalid() {
         given()
                 .when().get("/api/repositories/search?pidSystems=DOI")
                 .then()
                 .statusCode(401);
+    }
+
+    @Test
+    @TestSecurity(user = "userJwt", roles = "user")
+    @JwtSecurity(claims = {
+            @Claim(key = "tissID", value = "012345")
+    })
+    public void testRepositoriesEndpoint_Valid() {
+        given()
+                .when().get("/api/repositories")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @TestSecurity(user = "userJwt", roles = "user")
+    @JwtSecurity(claims = {
+            @Claim(key = "tissID", value = "012345")
+    })
+    public void testRepositoriesRecommendedEndpoint_Valid() {
+        given()
+                .when().get("/api/repositories/recommended")
+                .then()
+                .statusCode(200)
+                .body("id", hasItem("r3d100013557"));
+    }
+
+    @Test
+    @TestSecurity(user = "userJwt", roles = "user")
+    @JwtSecurity(claims = {
+            @Claim(key = "tissID", value = "012345")
+    })
+    public void testRepositoryByIdEndpoint_Valid() {
+        given()
+                .when().get("/api/repositories/r3d100013557")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @TestSecurity(user = "userJwt", roles = "user")
+    @JwtSecurity(claims = {
+            @Claim(key = "tissID", value = "012345")
+    })
+    public void testRepositoriesSearchEndpoint_Valid() {
+        given()
+                .when().get("/api/repositories/search?pidSystems=DOI")
+                .then()
+                .statusCode(200);
     }
 
 }
