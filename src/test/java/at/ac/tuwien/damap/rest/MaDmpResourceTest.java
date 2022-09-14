5065a760751e6257155f2f0cd2b42a85467b9ea7
Zeno Casellato <zeno.casellato@tuwien.ac.at>
2022-09-14 14:00:19 +0200

    create madmp tests,
    refs #77459

---
+++ src/test/java/at/ac/tuwien/damap/rest/MaDmpResourceTest.java
@@ -0,0 +1,93 @@
+package at.ac.tuwien.damap.rest;
+
+import at.ac.tuwien.damap.rest.dmp.domain.DmpDO;
+import at.ac.tuwien.damap.rest.projects.MockProjectServiceImpl;
+import at.ac.tuwien.damap.security.SecurityService;
+import at.ac.tuwien.damap.util.TestDOFactory;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.quarkus.test.security.TestSecurity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.inject.Inject;
+
+import static io.restassured.RestAssured.given;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@QuarkusTest
+@TestHTTPEndpoint(MaDmpResource.class)
+public class MaDmpResourceTest {
+
+    @Inject
+    TestDOFactory testDOFactory;
+
+    @InjectMock
+    SecurityService securityService;
+
+    @InjectMock
+    MockProjectServiceImpl mockProjectRestService;
+
+    @BeforeEach
+    public void setup() {
+        Mockito.when(securityService.getUserId()).thenReturn("012345");
+        Mockito.when(securityService.getUserName()).thenReturn("testUser");
+        Mockito.when(mockProjectRestService.getProjectDetails(anyString())).thenReturn(testDOFactory.getTestProjectDO());
+    }
+
+    @Test
+    public void testGetByIdEndpoint_Invalid() {
+        given()
+                .when().get("/0")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    @TestSecurity(user = "userJwt", roles = "user")
+    public void testGetByIdEndpoint_Unauthorized() {
+        given()
+                .when().get("/0")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    @TestSecurity(user = "userJwt", roles = "user")
+    public void testGetByIdEndpoint_Valid() {
+        DmpDO dmpDO = testDOFactory.getOrCreateTestDmpDO();
+        given()
+                .when().get("/" + dmpDO.getId())
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGetFileByIdEndpoint_Invalid() {
+        given()
+                .when().get("/file/0")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    @TestSecurity(user = "userJwt", roles = "user")
+    public void testGetFileByIdEndpoint_Unauthorized() {
+        given()
+                .when().get("/file/0")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    @TestSecurity(user = "userJwt", roles = "user")
+    public void testGetFileByIdEndpoint_Valid() {
+        DmpDO dmpDO = testDOFactory.getOrCreateTestDmpDO();
+        given()
+                .when().get("/file/" + dmpDO.getId())
+                .then()
+                .statusCode(200);
+    }
+}
