ec5ccaade4acacaacaea926acca3e221e489299d
Clara Schuster <98585998+clarascu@users.noreply.github.com>
2023-02-22 10:14:13 +0100

    Add GDPR utility (#80)
    * Add GDPR utility
    * Add GdprContext and refactor
    * Add lombok.config, so that JaCoCo ignores Lombok-generated code in test coverage reports.

--- src/main/java/at/ac/tuwien/damap/domain/Contributor.java
+++ src/main/java/at/ac/tuwien/damap/domain/Contributor.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Version;
 
+import at.ac.tuwien.damap.annotations.gdpr.*;
 import org.hibernate.envers.Audited;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -22,6 +23,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Setter;
 import lombok.ToString;
 
+@Gdpr
 @Data
 @EqualsAndHashCode(callSuper = true, exclude = "dmp")
 @ToString(exclude = "dmp")
@@ -33,34 +35,43 @@ public class Contributor extends PanacheEntity {
     @Setter(AccessLevel.NONE)
     private long version;
 
+    @GdprContext(properties = {"id", "project.title"})
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dmp_id", nullable = false, updatable = false)
     private Dmp dmp;
 
+    @GdprBase
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "person_id")
     private Identifier personIdentifier;
 
+    @GdprKey
     @Column(name = "university_id")
     private String universityId;
 
+    @GdprBase
     private String mbox;
 
+    @GdprBase
     @Column(name = "first_name")
     private String firstName;
 
+    @GdprBase
     @Column(name = "last_name")
     private String lastName;
 
+    @GdprExtended
     private String affiliation;
 
+    @GdprExtended
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "affiliation_id")
     private Identifier affiliationId;
 
     private Boolean contact;
 
+    @GdprExtended
     @Enumerated(EnumType.STRING)
     @Column(name = "contributor_role")
     private EContributorRole contributorRole;
