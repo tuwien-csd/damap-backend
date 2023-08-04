f8be5a618c5bb026dcfe0497bdd92b51c64f77ad
lpandath <112856634+lpandath@users.noreply.github.com>
2023-08-04 15:42:21 +0200

    minor changes urls

--- src/main/java/at/ac/tuwien/damap/enums/ELicense.java
+++ src/main/java/at/ac/tuwien/damap/enums/ELicense.java
@@ -13,13 +13,13 @@ public enum ELicense {
     CCBYNC("CC BY-NC 4.0", "Creative Commons Attribution-NonCommercial", "https://creativecommons.org/license/by-nc/4.0/"),
     CCBYNCSA("CC BY-NC-SA 4.0", "Creative Commons Attribution-NonCommercial-ShareAlike", "https://creativecommons.org/license/by-nc-sa/4.0/"),
     CCBYNCND("CC BY-NC-ND 4.0", "Creative Commons Attribution-NonCommercial-NoDerivs", "https://creativecommons.org/license/by-nc-nd/4.0/"),
-    PERLARTISTIC1("Artistic License 1.0", "Artistic License (Perl) 1.0", "https://opensource.org/license/Artistic-Perl-1.0"),
-    PERLARTISTIC2("Artistic-2.0", "Artistic License 2.0", "https://opensource.org/license/artistic-license-2-0-php/"),
+    PERLARTISTIC1("Artistic License 1.0", "Artistic License (Perl) 1.0", "https://opensource.org/license/artistic-perl-1-0-2/"),
+    PERLARTISTIC2("Artistic-2.0", "Artistic License 2.0", "https://opensource.org/license/artistic-2-0/"),
     GPL2PLUS("GPL-2.0", "GNU General Public License 2 or later", "https://opensource.org/license/GPL-2.0"),
     GPL2("GPL-2.0", "GNU General Public License 2", "https://opensource.org/license/GPL-2.0"),
     GPL3("GPL-3.0", "GNU General Public License 3", "https://opensource.org/license/GPL-3.0"),
-    AGPL1("AGPL-1.0", "Affero General Public License 1", ""),
-    AGPL3("AGPL-3.0", "Affero General Public License 3", "https://opensource.org/license/AGPL-3.0"),
+    AGPL1("AGPL-1.0", "Affero General Public License 1", "http://www.affero.org/oagpl.html"),
+    AGPL3("AGPL-3.0", "Affero General Public License 3", "https://opensource.org/license/agpl-v3/"),
     MPL2("MPL-2.0", "Mozilla Public License 2.0", "https://opensource.org/license/MPL-2.0"),
     LGPL21PLUS("LGPL-2.1", "GNU Library or \"Lesser\" General Public License 2.1 or later", "https://opensource.org/license/LGPL-2.1"),
     LGPL21("LGPL-2.1", "GNU Library or \"Lesser\" General Public License 2.1", "https://opensource.org/license/LGPL-2.1"),
@@ -29,7 +29,7 @@ public enum ELicense {
     MIT("MIT", "The MIT License", "https://opensource.org/license/mit-license.php"),
     BSD3C("BSD-3-Clause", "The BSD 3-Clause \"New\" or \"Revised\" License", "https://opensource.org/license/BSD-3-Clause"),
     BSD2C("BSD-2-Clause", "The BSD 2-Clause \"Simplified\" or \"FreeBSD\" License", "https://opensource.org/license/BSD-2-Clause"),
-    APACHE2("Apache-2.0", "Apache License 2", "https://www.apache.org/license/LICENSE-2.0");
+    APACHE2("Apache-2.0", "Apache License 2", "https://www.apache.org/licenses/LICENSE-2.0");
 
     private final String acronym;
     private final String name;
